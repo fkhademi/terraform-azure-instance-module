ab9606dd5eb3837b3644c866bc322a4bd66c24a0
Frey Khademi <49865527+fkhademi@users.noreply.github.com>
2022-04-08 15:10:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ resource "azurerm_virtual_machine" "instance" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
