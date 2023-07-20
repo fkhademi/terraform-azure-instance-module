776493caeb8fbc1fef03195e377e7f92c7f15512
Frey Khademi <49865527+fkhademi@users.noreply.github.com>
2023-07-20 22:56:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,19 @@ resource "azurerm_network_security_group" "default" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   security_rule {
     name                       = "HTTPS"
     priority                   = 1005
